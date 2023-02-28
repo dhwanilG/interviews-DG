da99cd306deb01bb573e860d5af2e0a5cd93b476
dhwanilG <94906946+dhwanilG@users.noreply.github.com>
2023-02-28 14:40:02 +0530

    Interview Changes
    Interview Changes for requirement

--- force-app/main/default/lwc/editQuote/editQuote.js
+++ force-app/main/default/lwc/editQuote/editQuote.js
@@ -3,14 +3,53 @@
  * Copyright (c) 2023 Provus Inc. All rights reserved.
  */
 
-import { LightningElement, api } from "lwc";
+import { LightningElement, api, track } from "lwc";
+import getQuoteDetails from '@salesforce/apex/EditQuoteController.getQuoteDetails';
+import saveQuote from '@salesforce/apex/EditQuoteController.saveQuote';
 
 export default class EditQuote extends LightningElement {
   @api recordId;
-  quoteData = {
+  
+  @track quoteData = {
     name: "Quote Name",
-    endDate: 1547250828000
+    endDate: 1547250828000,
+    startDate : 1547250828000
   };
 
   renderedCallback() {}
+
+  connectedCallback()
+  {
+    debugger;
+    getQuoteDetails({
+      recordId : this.recordId
+    })
+    .then(result =>{
+      if(result)
+      {
+        this.quoteData.id = result.id;
+        this.quoteData.name = result.name;
+        this.quoteData.endDate = result.endDate;
+        this.quoteData.startDate = result.startDate;
+        debugger;
+      }
+    })
+  }
+
+  handleFieldChange(event)
+  {
+    this.quoteData[event.target.name] = event.target.value;
+  }
+
+  handleSave()
+  {
+    debugger;
+    saveQuote({
+      objDto : this.quoteData
+    })
+    .then(result =>{
+       alert('Data Updated');
+    })
+    objDto
+  }
 }
